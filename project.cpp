846d1b9bba25efd56ed711b8e9f0ceab9300499a
hoanghainam <namprohang2@gmail.com>
2023-11-13 13:38:20 +0700

    hoanthien

--- project.cpp
+++ project.cpp
@@ -6,3 +6,12 @@ int main(){
     cout << "\nNhap so nguyen n = ";
 	cin >> n;
 }
+bool hoanthien(int a){
+    int sum = 0;
+    for(int i=1;i<=a/2;i++){
+        if(a%i==0) 
+            sum+=i;
+    }
+    if(sum==a) return true; 
+    return false;
+}
