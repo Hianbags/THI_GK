ce73a151b11e9fe08ebcd97a2ff2359d9ed588c7
hoanghainam <namprohang2@gmail.com>
2023-11-13 14:10:30 +0700

    sua main

--- project.cpp
+++ project.cpp
@@ -5,14 +5,15 @@ int main(){
     int n;
     cout << "\nNhap so nguyen n = ";
 	cin >> n;
-}
-bool chinhphuong(int n){
-int i = 0;
-	while(i*i <= n){
-		if(i*i == n){
-			return true;
-		}
-		++i;
-	}
-	return false;
+	cout<<"\n----------------------------------\n";
+	if(hoanthien(n) )
+        cout<<n<<" là số hoàn hảo.";
+    else
+        cout<<n<<" không phải là số hoàn hảo.";
+	 cout<<"\n----------------------------------\n";
+	if (chinhphuong(n))
+		cout<<n<<" là số chính phương.";
+    else
+        cout<<n<<" không phải là số chính phương.";
+	
 }
